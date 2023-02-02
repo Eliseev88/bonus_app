86cedb51bdfd85b51a1b80c9db03446352878a19
Igor <eliseev88@gmail.com>
2023-02-02 18:09:16 +0300

    some fixes

--- src/components/Bonus/Bonus.tsx
+++ src/components/Bonus/Bonus.tsx
@@ -28,7 +28,7 @@ export const Bonus = () => {
                 "sourceQuery": 0
             })
         };
-        fetch('/api/v3/clients/accesstoken', requestOptions)
+        fetch('https://mp1.iprobonus.com/api/v3/clients/accesstoken', requestOptions)
             .then(response => response.json())
             .then(data => setToken(data.accessToken));
     }, []);
@@ -40,7 +40,7 @@ export const Bonus = () => {
                 'AccessKey': process.env.REACT_APP_AccessKey,
                 'AccessToken': token,
             }
-            fetch(`api/v3/ibonus/generalinfo/${token}`, { headers })
+            fetch(`https://mp1.iprobonus.com/api/v3/ibonus/generalinfo/${token}`, { headers })
                 .then(response => response.json())
                 .then(data => setBonus({
                     currentQuantity: data.data.currentQuantity,
